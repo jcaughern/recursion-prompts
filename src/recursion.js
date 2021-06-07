da16afceb0e5a1ff987502e4276f79515c0bda83
Jake Caughern <jake.caughern@gmail.com>
2021-06-07 11:44:57 -0500

    Implemeted arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -35,6 +35,17 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var result = 0;
+
+  array.forEach(function(item){
+    if (!Array.isArray(item)) {
+      result += item;
+    } else {
+      result += arraySum(item);
+    }
+  });
+
+  return result;
 };
 
 // 4. Check if a number is even.
